d09b1eccb8807276da353f4c3be4b960d33cf0aa
victor99a <vi.barreraj@duocuc.cl>
2025-11-20 22:38:32 -0300

    correccion

--- src/main/java/com/victor/catalog/controller/ProductoController.java
+++ src/main/java/com/victor/catalog/controller/ProductoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/productos")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5174"})
 public class ProductoController {
     private final ProductoService productoService;
 
