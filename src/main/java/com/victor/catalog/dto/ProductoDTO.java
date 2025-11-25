c6395c42f41ce80f9eefc01400ce7b3983a2286a
victor99a <vi.barreraj@duocuc.cl>
2025-11-24 21:39:08 -0300

    Actualización interna: Cambios de Swagger implementados

--- src/main/java/com/victor/catalog/dto/ProductoDTO.java
+++ src/main/java/com/victor/catalog/dto/ProductoDTO.java
@@ -1,10 +1,22 @@
 package com.victor.catalog.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Información de un producto del catálogo.")
 public record ProductoDTO(
+        @Schema(description = "Identificador único del producto.", example = "1")
         Long id,
+
+        @Schema(description = "Nombre visible del producto.", example = "Torta Mil Hojas")
         String nombreProducto,
+
+        @Schema(description = "Descripción corta del producto.", example = "Torta de milhojas con manjar.")
         String descripcionProducto,
+
+        @Schema(description = "Precio de venta en pesos chilenos.", example = "15000")
         double precioProducto,
+
+        @Schema(description = "Categoria disponible", example = "7")
         Long categoriaId
 )
 {}
